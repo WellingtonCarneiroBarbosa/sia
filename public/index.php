55af4026eade3cd4b387afb3c832698f2040b158
WellingtonCarneiroBarbosa <carneirobarbosawellington@gmail.com>
2020-04-10 22:57:21 -0300

    update public/index.php

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . "/../vendor/autoload.php";
 
 /*
 |--------------------------------------------------------------------------
