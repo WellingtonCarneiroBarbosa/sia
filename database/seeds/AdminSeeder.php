142873188d82f107236d03109449c58311118ddf
WellingtonCarneiroBarbosa <carneirobarbosawellington@gmail.com>
2020-04-10 23:28:23 -0300

    create admin and user seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@exemple.org',
+            'role_id' => '5',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'User',
+            'email' => 'user@exemple.org',
+            'role_id' => '3',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
