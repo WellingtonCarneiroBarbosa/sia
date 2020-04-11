74d23ab320f2e8d630d4f0f649d58c99bd0c66b2
WellingtonCarneiroBarbosa <carneirobarbosawellington@gmail.com>
2020-04-10 22:43:02 -0300

    auth laravel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('role_id', 1)->default('3');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
