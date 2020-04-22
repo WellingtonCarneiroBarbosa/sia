5b0e8710e0ffa16b7b5b971d4a00aaa58406b012
Wellington <carneirobarbosawellington@gmail.com>
2020-04-22 00:19:04 -0300

    corrigido bug do horario

--- config/app.php
+++ config/app.php
@@ -66,8 +66,7 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-
-    'timezone' => 'UTC',
+    'timezone' => 'America/Sao_Paulo',
 
     /*
     |--------------------------------------------------------------------------
