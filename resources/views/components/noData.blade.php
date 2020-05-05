f9c147aeb69e9f3904d691efc462dd4d20b57460
Wellington <carneirobarbosawellington@gmail.com>
2020-05-05 17:56:53 -0300

    atualizado componente sem dados

--- resources/views/components/noData.blade.php
+++ resources/views/components/noData.blade.php
@@ -1,4 +1,5 @@
 <center class="fadeInTransition">
     <h1>{{ $message }}</h1>
-    <img style="width: 100%; height: 100%;" src="{{ asset('dashboard/assets/img/brand/team/image.jpg') }}" alt="{{ __("team") }}">
+    <div class="espaco"></div>
+    <img style="width: 70%; " src="{{ asset('dashboard/assets/img/brand/team/image.jpg') }}" alt="{{ __("team") }}">
 </center>
