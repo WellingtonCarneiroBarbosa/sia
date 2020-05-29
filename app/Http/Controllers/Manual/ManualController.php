274d00a6ccd7da6f31b57349cc2de69c673efb18
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2020-05-29 15:57:18 -0300

    corrigido bug do controlador

--- app/Http/Controllers/Manual/ManualController.php
+++ app/Http/Controllers/Manual/ManualController.php
@@ -1,28 +1,22 @@
-[15:15] Wellington Carneiro Barbosa (Convidado)
 <?php
+
 namespace App\Http\Controllers\Manual;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 class ManualController extends Controller
 {
-public function index()
-{
-return view('app.dashboard.manual.index');
-}
+    public function index()
+    {
+        return view('app.dashboard.manual.index');
+    }
 
-
-/**
-
- 
-* Schedules Manual Controller
-
- 
-*
-
- 
-*/
-public function schedulesCreate()
-{
-return view('app.dashboard.manual.schedules.create');
-}
+    /**
+    * Schedules Manual Controller
+    *
+    */
+    public function schedulesCreate()
+    {
+        return view('app.dashboard.manual.schedules.create');
+    }
 }
