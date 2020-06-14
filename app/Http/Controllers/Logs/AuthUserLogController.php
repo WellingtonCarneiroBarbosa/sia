1135f67741a27cb5b6aaad69c9faaf66b35560cd
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2020-06-14 07:01:55 -0300

    concluido visualizar minhas atividades

--- app/Http/Controllers/Logs/AuthUserLogController.php
+++ app/Http/Controllers/Logs/AuthUserLogController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Schedules\ScheduleLog;
 use App\Models\Places\PlaceLog;
 use App\Models\Customers\CustomerLog;
+use App\UserLog;
 use Auth;
 
 class AuthUserLogController extends Controller
@@ -21,7 +22,7 @@ class AuthUserLogController extends Controller
 
         $limit_per_log = 10;
 
-        $max_quantity_logs = $limit_per_log * 3;
+        $max_quantity_logs = $limit_per_log * 4;
 
         /**
          * Get latest logs on all
@@ -34,12 +35,14 @@ class AuthUserLogController extends Controller
 
         $customers_log = CustomerLog::where('user_id', $user_id)->latest()->take($limit_per_log)->get();
 
-        $quantity_logs = count($schedules_log) + count($places_log) + count($customers_log);
+        $users_log = UserLog::where('user_action_id', $user_id)->latest()->take($limit_per_log)->get();
+
+        $quantity_logs = count($schedules_log) + count($places_log) + count($customers_log) + count($users_log);
 
         return view('app.dashboard.logs.auth.index', [
             'schedules_log' => $schedules_log, 'places_log'    => $places_log,
             'customers_log' => $customers_log, 'quantity_logs' => $quantity_logs,
-            'max_quantity_logs' => $max_quantity_logs,
+            'users_log'     => $users_log,     'max_quantity_logs' => $max_quantity_logs,
         ]);
     }
 }
--- resources/views/app/dashboard/logs/auth/index.blade.php
+++ resources/views/app/dashboard/logs/auth/index.blade.php
@@ -77,63 +77,103 @@
                 break;
         } 
     }
+
+    /**
+     * Trata os dados dos logs
+     * dos usuarios
+     *
+     */
+    foreach($users_log as $log)
+    {
+        switch($log->action)
+        {
+            case 1: 
+                $log->action = $created;
+                break;
+            case 2: 
+                $log->action = $updated;
+                break;
+            case 3:
+                $log->action = Lang::get('deactivated');
+                break;
+            case 4: 
+                $log->action = Lang::get('enabled');
+                break;
+        } 
+    }
 @endphp 
 
 @section('content')
 <div class="container">
     <div class="espaco"></div>
-    @if(count($schedules_log) > 0)
+    @if($quantity_logs > 0)
         <h1>{{ __('Listing your latest') }} {{ $quantity_logs }} {{ __('system activities - max') }}: {{ $max_quantity_logs }}</h1>
     
-        <hr>
-        <ul>
-            @foreach ($schedules_log as $log)
-            <li>{{ __("You") }}
-                <strong>{{ $log->action }}</strong>
-                {{ __("a schedule") }} <u>{{ $log->created_at->diffForHumans() }}</u>
-                -
-                @if(isset($log->schedule_id) && isset($log->schedule_log['id']))
-                    <a href="{{ route('schedules.show', ['id' => $log->schedule_id]) }}">Visualizar agendamento</a>
-                @elseif(isset($log->schedule_id))
-                    <a href="{{ route('schedules.historic.show', ['id' => $log->schedule_id]) }}">Visualizar agendamento</a>
-                @else
-                    <i>Visualização não disponível</i>
-                @endif
-            </li>
-            @endforeach
-        </ul>
- 
-
-
-        <hr>
-        <ul>
-            @foreach ($customers_log as $log)
-            <li>{{ __("You") }}
-                <strong>{{ $log->action }}</strong>
-                {{ __("a customer") }} <u>{{ $log->created_at->diffForHumans() }}</u>
-                -
-                <a href="{{ route('customers.show', ['id' => $log->customer_id]) }}">Visualizar cliente</a>
-            </li>
-            @endforeach
-        </ul>
-  
-
-
-        <hr>
-        <ul>
-            @foreach ($places_log as $log)
-            <li>{{ __("You") }}
-                <strong>{{ $log->action }}</strong>
-                {{ __("a place") }} <u>{{ $log->created_at->diffForHumans() }}</u>
-                -
-                @if($log->place_id)
-                    <a href="{{ route('places.show', ['id' => $log->place_id]) }}">Visualizar local</a>
-                @else
-                    <i>Visualização não disponível</i>
-                @endif
+        @if(count($schedules_log) > 0)
+            <hr>
+            <ul>
+                @foreach ($schedules_log as $log)
+                <li>{{ __("You") }}
+                    <strong>{{ $log->action }}</strong>
+                    {{ __("a schedule") }} <u>{{ $log->created_at->diffForHumans() }}</u>
+                    -
+                    @if(isset($log->schedule_id) && isset($log->schedule_log['id']))
+                        <a href="{{ route('schedules.show', ['id' => $log->schedule_id]) }}">Visualizar agendamento</a>
+                    @elseif(isset($log->schedule_id))
+                        <a href="{{ route('schedules.historic.show', ['id' => $log->schedule_id]) }}">Visualizar agendamento</a>
+                    @else
+                        <i>Visualização não disponível</i>
+                    @endif
+                </li>
                 @endforeach
-            </li>   
-        </ul>
+            </ul>
+        @endif
+
+        @if(count($customers_log) > 0)
+            <hr>
+            <ul>
+                @foreach ($customers_log as $log)
+                <li>{{ __("You") }}
+                    <strong>{{ $log->action }}</strong>
+                    {{ __("a customer") }} <u>{{ $log->created_at->diffForHumans() }}</u>
+                    -
+                    <a href="{{ route('customers.show', ['id' => $log->customer_id]) }}">Visualizar cliente</a>
+                </li>
+                @endforeach
+            </ul>
+        @endif
+            
+        @if(count($places_log) > 0)
+            <hr>
+            <ul>
+                @foreach ($places_log as $log)
+                <li>{{ __("You") }}
+                    <strong>{{ $log->action }}</strong>
+                    {{ __("a place") }} <u>{{ $log->created_at->diffForHumans() }}</u>
+                    -
+                    @if(isset($log->place_id))
+                        <a href="{{ route('places.show', ['id' => $log->place_id]) }}">Visualizar local</a>
+                    @else
+                        <i>Visualização não disponível</i>
+                    @endif
+                    @endforeach
+                </li>   
+            </ul>
+        @endif
+
+        @if(count($users_log) > 0)
+            <hr>
+            <ul>
+                @foreach ($users_log as $log)
+                <li>{{ __("You") }}
+                    <strong>{{ $log->action }}</strong>
+                    {{ __("a user") }} <u>{{ $log->created_at->diffForHumans() }}</u>
+                    -
+                    <a href="{{ route('users.show', ['id' => $log->user_id]) }}">Visualizar usuário</a>
+                    @endforeach
+                </li>   
+            </ul>
+        @endif
     @else 
         @component('components.noData', ['message' => Lang::get('You have not yet performed any activity on the system')])@endcomponent
     @endif
