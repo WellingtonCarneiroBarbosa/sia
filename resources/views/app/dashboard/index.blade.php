65281bac4658791c6d93c04e8e9e24101aaea9e1
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2020-06-28 19:11:38 -0300

    finalizado implementacao do full calendar

--- resources/views/app/dashboard/index.blade.php
+++ resources/views/app/dashboard/index.blade.php
@@ -8,7 +8,7 @@
     <script src='{{ asset("dashboard/libs/full-calendar/lib/main.js") }}'></script>
     <script src="{{ asset('dashboard/libs/full-calendar/lib/locales/pt-br.js') }}"></script>
     <script>
-    document.addEventListener('DOMContentLoaded', function() {
+    $(document).ready(function () { 
         var calendarEl = document.getElementById('calendar');
         var data = "{{ $schedules }}";
 
@@ -20,12 +20,26 @@
             locale: "pt-br",
             @endif
 
-            // Redirect to more details on click
+            // Show modal action options
             eventClick: function(info) {
-                var showScheduleInfosUrl = "{{ url('dash/schedules/show') }}";
-                var id = info.event.id
-                showScheduleInfosUrl = showScheduleInfosUrl + "/" + id;
-                return window.open(showScheduleInfosUrl, '_blank');
+                // Get schedule ID 
+                var scheduleID = info.event.id 
+
+                // Get schedule action endpoints
+                var showSchedule = "{{ url('dash/schedules/show') }}" + "/" + scheduleID;
+                var editSchedule = "{{ url('dash/schedules/edit') }}" + "/" + scheduleID;
+                var cancelSchedule = "{{ url('dash/schedules/confirm/cancel') }}" + "/" + scheduleID;
+
+                //Set urls on modal buttons
+                document.getElementById('show-schedule').href = showSchedule;
+                document.getElementById('edit-schedule').href = editSchedule;
+                document.getElementById('cancel-schedule').href = cancelSchedule;
+                
+                // Scroll to the modal
+                window.scrollTo(0, 0);
+
+                //Show modal
+                return $('#modal-action').modal();
             },
 
             headerToolbar: {
@@ -62,7 +76,7 @@
                     <a href="{{route('schedules.canceled')}}" class="btn btn-sm btn-neutral mb-2">{{ __("Canceled") }}</a>
                     <a href="#" data-toggle="modal" data-target="#modal-filter" id="filtros-agendamento" class="btn btn-sm btn-neutral mb-2 mr-2">{{ __("Filters") }}</a>
                     {{-- <a href="{{ route('schedules.generate.guestURL') }}" class="btn btn-sm btn-neutral mb-2">Gerar compartilhamento temporário</a> --}}
-                    <a href="{{ route('schedules.historic.index') }}" class="btn btn-sm btn-neutral mb-2">Histórico</a>
+                    <a href="{{ route('schedules.historic.index') }}" class="btn btn-sm btn-neutral mb-2">{{ __("Historic") }}</a>
                 </div>
             </div>
             <!-- fim do header -->
@@ -88,6 +102,42 @@
 
     @component('components.modals.findSchedule', ['hasPlaces' => $hasPlaces, 'places' => $places])@endcomponent
 
+    {{-- Action Modal --}}
+    <div class="modal-filtros fade" id="modal-action" tabindex="-1" role="dialog" aria-labelledby="modal-action" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="espacol"></div>
+                <div class="text-center">
+                    <h3>{{ __("Selecione Uma Ação") }}</h3>
+                </div>
+                <div class="modal-body">
+                    <!--options-->
+    
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="tab-content" id="myTabContent">
+                                <center>
+                                    <a href="#" id="show-schedule">
+                                        <button class="btn btn-primary">{{ __("View more") }}</button>
+                                    </a>
+                                    <a href="#" id="edit-schedule">
+                                        <button class="btn btn-primary">{{ __("Edit") }}</button>
+                                    </a>
+                                    <a href="#" id="cancel-schedule">
+                                        <button class="btn btn-primary">{{ __("Cancel") }}</button>
+                                    </a>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end options-->
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-primary  ml-auto" data-dismiss="modal">{{ __("Close") }}</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
 
