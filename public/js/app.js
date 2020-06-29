53a7911245942cc6125b961c800383b72fbd5665
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2020-06-29 09:45:20 -0300

    node_modules

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/sia/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/sia/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /opt/lampp/htdocs/projects/sia/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /opt/lampp/htdocs/projects/sia/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
