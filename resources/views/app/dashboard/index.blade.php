9a71f4df7a5ea2fc8bfc8dbb4f01b28aaa4ee333
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2020-06-30 02:40:37 -0300

    loader

--- resources/views/app/dashboard/index.blade.php
+++ resources/views/app/dashboard/index.blade.php
@@ -9,102 +9,108 @@
     <script src="{{ asset('dashboard/libs/full-calendar/lib/locales/pt-br.js') }}"></script>
 
     <script>
-    $(document).ready(function () { 
-        function formatDate (input) {
-            var datePart = input.match(/\d+/g),
-            year = datePart[0].substring(2), // get only two digits
-            month = datePart[1], day = datePart[2];
-          
-            return day+'/'+month+'/'+year;
-        }
+        $(document).ready(function () { 
+            $("#pageloader").show();
 
-        var calendarEl = document.getElementById('calendar');
-        var data = "{{ $schedules }}";
-
-        data = data.replace(/&quot;/g,'"')
-        //if schedule confirmed, color = green
-        data = data.replace(/"status":"1"/g, '"color":"#28a745"')
-        //if schedule on budge, color = orange
-        data = data.replace(/"status":null/g, '"color":"#ffc107"')
-     
-        var calendar = new FullCalendar.Calendar(calendarEl, {
+            function formatDate (input) {
+                var datePart = input.match(/\d+/g),
+                year = datePart[0].substring(2), // get only two digits
+                month = datePart[1], day = datePart[2];
             
-            @if(config('app.locale') != "pt-BR")
-            locale: "en",
-            @else 
-            locale: "pt-br",
-            @endif
+                return day+'/'+month+'/'+year;
+            }
 
-            // Show modal action options
-            eventClick: function(info) {
+            var calendarEl = document.getElementById('calendar');
+            var data = "{{ $schedules }}";
 
-                // Get schedule ID 
-                var scheduleID = info.event.id 
+            data = data.replace(/&quot;/g,'"')
+            //if schedule confirmed, color = green
+            data = data.replace(/"status":"1"/g, '"color":"#28a745"')
+            //if schedule on budge, color = orange
+            data = data.replace(/"status":null/g, '"color":"#ffc107"')
+        
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                
+                @if(config('app.locale') != "pt-BR")
+                locale: "en",
+                @else 
+                locale: "pt-br",
+                @endif
 
-                // Get schedule action endpoints
-                var showSchedule = "{{ url('dash/schedules/show') }}" + "/" + scheduleID;
-                var editSchedule = "{{ url('dash/schedules/edit') }}" + "/" + scheduleID;
-                var cancelSchedule = "{{ url('dash/schedules/confirm/cancel') }}" + "/" + scheduleID;
+                // Show modal action options
+                eventClick: function(info) {
+                    $("#pageloader").show();
+                    // Get schedule ID 
+                    var scheduleID = info.event.id 
 
-                // Get schedule infos 
-                var scheduleInfoUrl = "{{ url('dash/schedules/info') }}" + "/" + scheduleID
+                    // Get schedule action endpoints
+                    var showSchedule = "{{ url('dash/schedules/show') }}" + "/" + scheduleID;
+                    var editSchedule = "{{ url('dash/schedules/edit') }}" + "/" + scheduleID;
+                    var cancelSchedule = "{{ url('dash/schedules/confirm/cancel') }}" + "/" + scheduleID;
 
-                $(function(){
-                    $.ajax({
-                        url: scheduleInfoUrl,
-                        method: "GET", 
+                    // Get schedule infos 
+                    var scheduleInfoUrl = "{{ url('dash/schedules/info') }}" + "/" + scheduleID
 
-                        success: function (data) {
-                            var place = data.scheduling_place.name
-                            var customer = data.scheduling_customer.corporation
-                            var start = data.start
-                            var end = data.end 
-                            var details = data.details
-                            if(details == null) 
-                                details = "{{ Lang::get('Nothing to show') }}"
+                    $(function(){
+                        $.ajax({
+                            url: scheduleInfoUrl,
+                            method: "GET", 
 
-                             // get modal options 
-                            document.getElementById('schedule-modal-title').innerHTML = info.event.title;
-                            document.getElementById('schedule-modal-place').innerHTML = place;
-                            document.getElementById('schedule-modal-start').innerHTML = start;
-                            document.getElementById('schedule-modal-end').innerHTML = end;
-                            document.getElementById('schedule-modal-customer').innerHTML = customer;
-                            document.getElementById('schedule-modal-details').innerHTML = details;
-                        }
+                            success: function (data) {
+                                var place = data.scheduling_place.name
+                                var customer = data.scheduling_customer.corporation
+                                var start = data.start
+                                var end = data.end 
+                                var details = data.details
+                                if(details == null) 
+                                    details = "{{ Lang::get('Nothing to show') }}"
+
+                                // get modal options 
+                                document.getElementById('schedule-modal-title').innerHTML = info.event.title;
+                                document.getElementById('schedule-modal-place').innerHTML = place;
+                                document.getElementById('schedule-modal-start').innerHTML = start;
+                                document.getElementById('schedule-modal-end').innerHTML = end;
+                                document.getElementById('schedule-modal-customer').innerHTML = customer;
+                                document.getElementById('schedule-modal-details').innerHTML = details;
+                            }
+                        })
                     })
-                })
 
-                var title = info.event.title; 
-                var place = info.event.place_id;
+                    var title = info.event.title; 
+                    var place = info.event.place_id;
 
-                //Set urls on modal buttons
-                document.getElementById('show-schedule').href = showSchedule;
-                document.getElementById('edit-schedule').href = editSchedule;
-                document.getElementById('cancel-schedule').href = cancelSchedule;
+                    //Set urls on modal buttons
+                    document.getElementById('show-schedule').href = showSchedule;
+                    document.getElementById('edit-schedule').href = editSchedule;
+                    document.getElementById('cancel-schedule').href = cancelSchedule;
 
-                // Scroll to the modal
-                window.scrollTo(0, 0);
+                    // Scroll to the modal
+                    window.scrollTo(0, 0);
 
-                //Show modal
-                return $('#modal-action').modal();
-            },
+                    $("#pageloader").hide();
+                    //Show modal
+                    return $('#modal-action').modal();
+                },
 
-            headerToolbar: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-            },
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+                },
+                
+                initialDate: "{{ now() }}",
+                navLinks: true, // can click day/week names to navigate views
+                businessHours: true, // display business hours
+                editable: true,
+                selectable: true,
+                events: JSON.parse(data)
+            });
+
+            calendar.render()
             
-            initialDate: "{{ now() }}",
-            navLinks: true, // can click day/week names to navigate views
-            businessHours: true, // display business hours
-            editable: true,
-            selectable: true,
-            events: JSON.parse(data)
+            $("#pageloader").hide();
+            
         });
-
-        calendar.render();
-    });
     </script>
 @endsection
 
