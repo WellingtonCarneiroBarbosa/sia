fa28b1f97fce388fd95ff83a5b990b12bf7d04d2
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2020-07-01 20:29:54 -0300

    corrigido criacao de log ao alterar senha

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -50,7 +50,7 @@ class UserObserver
     public function updated(User $user)
     {
         /**Impede que o método seja utilizado caso a coluna password seja alterada */
-        if(! $schedule->isDirty($schedule->getPasswordColumn()) && count($schedule->getDirty()) != 1){
+        if(! $user->isDirty($user->getPasswordColumn()) && count($user->getDirty()) != 1){
             $log     =
             [
                 'user_id'        => $user->id,
